1df020a764c3db36c5b3f8d1fddbe2a6359e743b
John <101512527+Y0hn@users.noreply.github.com>
2024-01-31 22:37:56 +0100

    PoltarSoftworks

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tic-Tac-Big")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Big Game of Tic-Tac-Toe")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PoltarSoftworks")]
 [assembly: AssemblyProduct("Tic-Tac-Big")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
